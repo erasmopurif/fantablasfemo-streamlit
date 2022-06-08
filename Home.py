74dd33ab6861433b40b0cd0f4a124fda524fbe01
erasmopurif <erasmopurif@gmail.com>
2022-06-09 00:44:25 +0200

    Added multipage mode

---
+++ Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+# Title of the main page
+st.title("Fantablasfemo Manager")
+
+st.markdown(
+    """
+    ### *"La bestemmia è libera"*
+    ---
+    #### Riferimenti ufficiali del **Fantablasfemo Manager**:
+    * [Lega Fantacalcio.it](https://leghe.fantacalcio.it/fantablasfemo)
+    * [Regolamento](https://drive.google.com/file/d/17l29sVD2nuyYNJFIPYyuePiOj9ZVKGEH/view?usp=sharing) 
+    ---
+    ##### 👈 Funzionalità aggiuntive nel menu laterale.
+    """
+)
